f17a13dc3c869b6d38110a9b27d46c9450d28f50
Sunny-Baishnab <72726626+Sunny-Baishnab@users.noreply.github.com>
2021-05-31 18:19:21 +0530

    Add files via upload

---
+++ webscaper2.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup as BS
+import requests
+import pandas as pd
+
+url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+
+page = requests.get(url)
+
+soup = BS(page.text,'html.parser')
+star_table = soup.find('table')
+
+temp_list = []
+
+table_rows = star_table.find_all('tr')
+
+for tr in table_rows:
+    td = tr.find_all('td')
+    row= [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+star_names = []
+
+distance = []
+mass = []
+radius = []
+
+for i in range(1,len(temp_list)):
+    star_names.append(temp_list[i][0])
+    distance.append(temp_list[i][5])
+    mass.append(temp_list[i][8])
+    radius.append(temp_list[i][9])
+
+df = pd.DataFrame(list(zip(star_names,distance,mass,radius)),columns = ['star_names','distance','mass','radius'])
+df.to_csv('brownstars.csv')
